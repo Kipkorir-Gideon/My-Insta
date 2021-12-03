2c4e524f2f4fe716630f6d2c260037f93c3ebc54
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-03 19:46:49 +0300

    Create image model and it's proterties.

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=100)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes = models.ForeignKey(Likes, on_delete=models.CASCADE)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
