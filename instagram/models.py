fc5f8a202d6c0685806361bf50aad808fab0be73
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-03 21:46:27 +0300

    Add a function to create user profile everytime a user register.

--- instagram/models.py
+++ instagram/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 import datetime as dt
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Image(models.Model):
@@ -17,6 +19,11 @@ class Profile(models.Model):
     photo = CloudinaryField('photo')
     bio = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    @receiver(post_save, sender = User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
 
 class Comments(models.Model):
