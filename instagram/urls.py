37d7941c308af910acf0beeb99cbdf3864fd6e53
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-04 18:01:14 +0300

    Add a login path.

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns=[
     path('',views.homepage, name='homepage'),
     path('register/', views.register, name='register'),
+    path("login/", views.login, name="login")
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
