207e2ffa379c6d7a6d593912fa0cfbdf5158e668
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-06 19:08:47 +0300

    Add a form for posting images.

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, Comments
+from .models import *
 
 
 class NewUserForm(UserCreationForm):
@@ -29,6 +29,12 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('photo', 'user', 'bio')
+
+
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Image
+    fields = ['image', 'name', 'caption']
 
 
 class CommentForm(forms.ModelForm):
