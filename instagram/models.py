3e6611e5807b79fda8f178bb7a983d34d137197f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-07 15:58:05 +0300

    Fix a bug on models and templates.

--- instagram/models.py
+++ instagram/models.py
@@ -63,7 +63,7 @@ class Image(models.Model):
 
     @classmethod
     def search_images(cls, search_term):
-        images = cls.objects.filter(name__icontains=search_term.all())
+        images = cls.objects.filter(name__icontains=search_term).all()
         return images
 
     @property
